f2e0f8aaa6d62080def00a0422b6af03d4567ad0
gustavocarus0_ <gustavocaruso82@gmail.com>
2023-08-21 17:49:55 -0300

    update_lista4_5_6_7

--- Lista2-Questoes/exerc7_resp.php
+++ Lista2-Questoes/exerc7_resp.php
@@ -1,7 +1,6 @@
 <!doctype html>
 <html lang="en">
-
-<head>
+  <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,34 +8,17 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Lista 3 - Exercicio 2</title>
-</head>
-
-<body>
-<?php
-if (isset($_POST['valor1']) && isset($_POST['valor2'])) {
-    $valor1 = $_POST['valor1'];
-    $valor2 = $_POST['valor2'];
-
-    function calcularSoma($valor1, $valor2) {
-        $soma = $valor1 + $valor2;
-
-        if ($valor1 == $valor2) {
-            return $soma * 3;
-        } else {
-            return $soma;
-        }
+    <title>Exercício 7 - Lista 2</title>
+  </head>
+  <body class="container">
+  <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $metros = $_POST['metros'];
+        $centimetros = $metros * 100; // 1 metro = 100 centímetros
+        
+        echo "<p>$metros metros equivalem a $centimetros centímetros.</p>";
     }
-
-    $resultado = calcularSoma($valor1, $valor2);
-
-    echo "Resultado: $resultado";
-} else {
-    echo "Por favor, preencha ambos os valores.";
-}
-?>
-
-
+    ?>
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
@@ -47,6 +29,5 @@ if (isset($_POST['valor1']) && isset($_POST['valor2'])) {
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     -->
-</body>
-
+  </body>
 </html>
